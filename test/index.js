df8bede9136fd129680305249985efe3b5d28a9f
FokusDotId <mr.familia13@gmail.com>
2024-01-05 17:36:05 +0700

    add fonts noto sans

--- test/index.js
+++ test/index.js
@@ -1,4 +1,26 @@
+import FormData from 'form-data';
+import axios, { isAxiosError } from "axios";
 import QC from '../index.js';
+
+async function telegraph(buffer, options = {}) {
+	let filename = options.filename || options.fileName || options.name || options.Name || undefined;
+	const form = new FormData();
+	form.append('file', buffer, filename || 'image.png');
+	try {
+		const { data } = await axios({
+			url: "https://telegra.ph/upload",
+			method: 'POST',
+			data: form,
+			headers: form.getHeaders()
+		});
+		
+		if (!Array.isArray(data) || !data[0].src) throw "Failed to upload this file";
+		return "https://telegra.ph" + data[0].src;
+	} catch (e) {
+		if (isAxiosError(e)) e = e.response.data;
+		throw e;
+	};
+};
 
 const data = {
     type: 'quote',
@@ -28,5 +50,9 @@ const data = {
 };
 
 QC(data)
-    .then(console.log)
+    .then(async ({ image }) => {
+    	await telegraph(Buffer.from(image, 'base64'))
+    		.then(console.log)
+    		.catch(console.error);
+    })
     .catch(console.error);
